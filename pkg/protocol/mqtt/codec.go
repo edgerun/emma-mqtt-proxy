0df3e3fd5ed267ac669c1f7751b0cbdb3e553638
Thomas Rausch <thomas@rauschig.org>
2020-05-28 13:19:08 +0200

    Add basic parsing of mqtt packet header

--- pkg/protocol/mqtt/codec.go
+++ pkg/protocol/mqtt/codec.go
@@ -4,32 +4,52 @@ import (
 	"io"
 )
 
+func ReadPacketHeader(header *PacketHeader, reader io.Reader) (int, error) {
+	buf := []byte{0x0}
+
+	n, err := reader.Read(buf)
+
+	if err != nil {
+		return n, err
+	}
+	if n == 0 {
+		return n, nil
+	}
+
+	header.Type = (buf[0] >> 4) & 0xF
+	header.Flags = buf[0] & 0xF
+
+	length, nLength, err := ReadVariableByteUint32(reader) // todo blocking busy wait
+	header.RemainingLength = length
+	return n + nLength, err
+}
+
 // Reads from an io.Reader a uint32 encoded as variable byte integer in a maximum of 4 sequential bytes.
 // See https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901011
-func ReadVariableByteUint32(reader io.Reader) (uint32, error) {
+func ReadVariableByteUint32(reader io.Reader) (uint32, int, error) {
 	const cmask = 0b10000000 // 0x80 masks continuation bit
 	const dmask = 0b01111111 // 0x7F masks integer data
 
-	var pos uint8
+	var read int
 	var result uint32
 
 	buf := []byte{0x0}
-	for pos = 0; pos <= 4; pos++ {
+	for read = 0; read <= 4; read++ {
 		n, err := reader.Read(buf)
 		if err != nil {
-			return 0, err
+			return 0, read + n, err
 		}
 		if n != 1 {
 			continue // TODO: busy wait
 		}
 
 		b := buf[0]
-		result += uint32(b & dmask) << (7 * pos)
+		result += uint32(b&dmask) << (7 * read)
 
-		if b & cmask == 0 {
+		if b&cmask == 0 {
 			break
 		}
 	}
 
-	return result, nil
+	return result, read + 1, nil
 }
--- pkg/protocol/mqtt/codec_test.go
+++ pkg/protocol/mqtt/codec_test.go
@@ -6,34 +6,61 @@ import (
 	"testing"
 )
 
+func TestReadPacketHeader(t *testing.T) {
+	header := &PacketHeader{}
+
+	reader := bytes.NewReader([]byte{
+		0b11000000, // PINGREQ + no flags)
+		0b00000000, // remaining length 0,
+		0b11010000, // ... some next packet
+	})
+
+	read, err := ReadPacketHeader(header, reader)
+
+	if err != nil {
+		t.Error("Unexpected error", err)
+	}
+	if header.Type != TypePingReq {
+		t.Errorf("Expected packet type %s, got %s", "PINGREQ", PacketTypeName(header.Type))
+	}
+	if read != 2 {
+		t.Error("Expected to read 2 bytes from reader")
+	}
+
+}
+
 func TestReadVariableByteUint32(t *testing.T) {
 	// TODO: write more tests (+ error cases)
 
-	matrix := []struct{
-		reader io.Reader
-		expected uint32
+	matrix := []struct {
+		reader       io.Reader
+		expectedRead int
+		expected     uint32
 	}{
 		{
 			bytes.NewReader([]byte{
 				0b10000000, // cont
 				0b00000001, // 128
-			}), 128,
+			}), 2, 128,
 		},
 		{
 			bytes.NewReader([]byte{
 				0b00000001, // cont
-			}), 1,
+			}), 1, 1,
 		},
 	}
 
 	for _, testcase := range matrix {
-		actual, err := ReadVariableByteUint32(testcase.reader)
+		actual, read, err := ReadVariableByteUint32(testcase.reader)
 
 		if err != nil {
 			t.Error("Unexpected error", err)
 		}
 		if actual != testcase.expected {
 			t.Errorf("Expected val to be %d, was %d", testcase.expected, actual)
+		}
+		if read != testcase.expectedRead {
+			t.Errorf("Expected to read %d bytes, was %d", testcase.expectedRead, read)
 		}
 	}
 
